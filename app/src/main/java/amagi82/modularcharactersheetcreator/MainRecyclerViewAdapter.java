a7ca3b4b26c783bc8e6e56199ee55802fbe4ffde
Amagi82 <amagi82@gmail.com>
2015-05-05 15:14:46 -0700

    Character icons

--- app/src/main/java/amagi82/modularcharactersheetcreator/MainRecyclerViewAdapter.java
+++ app/src/main/java/amagi82/modularcharactersheetcreator/MainRecyclerViewAdapter.java
@@ -5,24 +5,26 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class MainRecyclerViewAdapter extends RecyclerView.Adapter<MainRecyclerViewAdapter.ViewHolder> {
+
     private ArrayList<Character> characters;
 
     // Provide a reference to the views for each data item
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView imageCharacterIcon;
+        CircleImageView imageCharacterIcon;
         TextView tvName;
         TextView tvCharacterClass;
         TextView tvGameSystem;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            imageCharacterIcon = (ImageView) itemView.findViewById(R.id.imageCharacterIcon);
+            imageCharacterIcon = (CircleImageView) itemView.findViewById(R.id.imageCharacterIcon);
             tvName= (TextView) itemView.findViewById(R.id.tvName);
             tvCharacterClass= (TextView) itemView.findViewById(R.id.tvCharacterClass);
             tvGameSystem= (TextView) itemView.findViewById(R.id.tvGameSystem);
@@ -43,8 +45,9 @@ public class MainRecyclerViewAdapter extends RecyclerView.Adapter<MainRecyclerVi
     // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        //TODO: Check if image is null. If so, replace with first letter of character and color of game system
-        holder.imageCharacterIcon.setImageBitmap(characters.get(position).getImageCharacterIcon());
+        if(characters.get(position).getImageCharacterIcon() == null) {
+            holder.imageCharacterIcon.setImageResource(R.drawable.ic_face_grey600_24dp);
+        }else holder.imageCharacterIcon.setImageBitmap(characters.get(position).getImageCharacterIcon());
         holder.tvName.setText(characters.get(position).getName());
         holder.tvCharacterClass.setText(characters.get(position).getCharacterClass());
         holder.tvGameSystem.setText(characters.get(position).getGameSystem());
