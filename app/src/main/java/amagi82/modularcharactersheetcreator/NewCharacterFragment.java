b58b2a0c010b9f17bc5b8396b0e5ee2fd51b9b38
Amagi82 <amagi82@gmail.com>
2015-05-08 18:11:02 -0700

    added data to smooth character creation

--- app/src/main/java/amagi82/modularcharactersheetcreator/NewCharacterFragment.java
+++ app/src/main/java/amagi82/modularcharactersheetcreator/NewCharacterFragment.java
@@ -14,6 +14,8 @@ import android.widget.ImageView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 
+import java.util.HashMap;
+
 public class NewCharacterFragment extends Fragment implements View.OnClickListener {
 
     private ImageView iconCharacter;
@@ -26,8 +28,7 @@ public class NewCharacterFragment extends Fragment implements View.OnClickListen
     private EditText etCharacterClass;
     private EditText etTemplate;
     private EditText etColor;
-    private int[] stringArrayIds = {}; //TODO: add list of string arrays
-    private int stringArrayId;
+    private HashMap<String, Integer> mapArrayIds;
 
     public NewCharacterFragment() {
     }
@@ -36,6 +37,23 @@ public class NewCharacterFragment extends Fragment implements View.OnClickListen
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_new_character, container, false);
         setHasOptionsMenu(true);
+
+        //Set up hashmap Ids
+        if(mapArrayIds == null) {
+            mapArrayIds = new HashMap<>();
+            int[] classIds = {R.array.classes_fantasy, R.array.classes_cthulhu, R.array.classes_fantasy, R.array.classes_dungeon_world,
+                    R.array.classes_earthdawn, R.array.classes_feng_shui, R.array.classes_gurps, R.array.classes_fantasy, R.array.classes_shadowrun};
+            int[] classIdsWod = {R.array.classes_cwod_vampire, R.array.classes_cwod_werewolf, R.array.classes_cwod_mage, R.array.classes_cwod_wraith,
+                    R.array.classes_cwod_changeling, R.array.classes_nwod_vampire, R.array.classes_nwod_werewolf, R.array.classes_nwod_mage};
+            String[] stringArray = getResources().getStringArray(R.array.game_systems);
+            for (int i = 0; i < stringArray.length - 1; i++) {
+                mapArrayIds.put(stringArray[i], classIds[i]);
+            }
+            stringArray = getResources().getStringArray(R.array.game_systems_wod);
+            for (int i = 0; i < stringArray.length - 1; i++) {
+                mapArrayIds.put(stringArray[i], classIdsWod[i]);
+            }
+        }
 
         getActivity().setTitle(getResources().getString(R.string.new_character));
 
@@ -61,11 +79,30 @@ public class NewCharacterFragment extends Fragment implements View.OnClickListen
         return rootView;
     }
 
+    void openMenu(final int arrayId, final EditText editText, final ImageView icon){
+        new MaterialDialog.Builder(getActivity()).items(arrayId).itemsCallback(new MaterialDialog.ListCallback() {
+            @Override
+            public void onSelection(MaterialDialog dialog, View view, int position, CharSequence text) {
+                if(!text.equals(getString(R.string.other))){
+                    if(editText.getText().toString().equals(getString(R.string.dnd))){
+                        editText.setText(editText.getText() + " " + text + " " + getString(R.string.edition));
+                    }else{
+                        editText.setText(text);
+                        icon.setVisibility(View.VISIBLE);
+                        if(text.equals(getString(R.string.dnd))) openMenu(R.array.game_systems_dnd, editText, icon);
+                        if(text.equals(getString(R.string.world_of_darkness))) openMenu(R.array.game_systems_wod, editText, icon);
+                    }
+                }
+            }
+        }).show();
+    }
+
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         menu.clear();
         inflater.inflate(R.menu.menu_new_character, menu);
     }
+
 
     @Override
     public void onClick(View v) {
@@ -74,21 +111,11 @@ public class NewCharacterFragment extends Fragment implements View.OnClickListen
                 Log.i(null, "character icon clicked");
                 break;
             case R.id.iconGameSystem:
-                new MaterialDialog.Builder(getActivity()).items(R.array.game_systems).itemsCallback(new MaterialDialog.ListCallback() {
-                    @Override
-                    public void onSelection(MaterialDialog dialog, View view, int position, CharSequence text) {
-                        etGameSystem.setText(getResources().getStringArray(R.array.game_systems)[position]);
-                        //stringArrayId = stringArrayIds[position];
-                    }
-                }).show();
+                openMenu(R.array.game_systems, etGameSystem, iconClass);
                 break;
             case R.id.iconClass:
-                new MaterialDialog.Builder(getActivity()).items(stringArrayId).itemsCallback(new MaterialDialog.ListCallback() {
-                    @Override
-                    public void onSelection(MaterialDialog dialog, View view, int position, CharSequence text) {
-                        etCharacterClass.setText(getResources().getStringArray(stringArrayId)[position]);
-                    }
-                }).show();
+                int id = mapArrayIds.get(getString(R.string.other));
+                openMenu(id, etCharacterClass, null);
                 break;
             case R.id.iconTemplate | R.id.etTemplate:
                 Log.i(null, "Offer template if available");
