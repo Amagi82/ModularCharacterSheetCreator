1dadadd4402a0c28accf405568b0e696485d4c5d
Amagi82 <amagi82@gmail.com>
2015-06-06 07:44:42 -0700

    Save photos

--- app/src/main/java/amagi82/modularcharactersheetcreator/fragments/CreateCharacterFragment.java
+++ app/src/main/java/amagi82/modularcharactersheetcreator/fragments/CreateCharacterFragment.java
@@ -1,7 +1,12 @@
 package amagi82.modularcharactersheetcreator.fragments;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -14,18 +19,23 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.edmodo.cropper.CropImageView;
+
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -47,6 +57,7 @@ public class CreateCharacterFragment extends Fragment implements Toolbar.OnMenuI
     private boolean isEditMode = false;
     private EditText editText;
     private Uri photoUri;
+    private CropImageView cropper;
 //    @InjectView(R.id.color_mask) View colorMask;
     @InjectView(R.id.toolbar) Toolbar toolbar;
     @InjectView(R.id.appbar) AppBarLayout appbar;
@@ -79,6 +90,8 @@ public class CreateCharacterFragment extends Fragment implements Toolbar.OnMenuI
         //collapsingToolbar.setTitle(gameCharacter.getCharacterName().length() < 1? getString(R.string.new_character) : gameCharacter.getCharacterName());
         toolbar.inflateMenu(isEditMode ? R.menu.menu_edit_character : R.menu.menu_new_character);
         toolbar.setOnMenuItemClickListener(this);
+
+        if(gameCharacter.getPortraitUri() != null) imagePortrait.setImageURI(gameCharacter.getPortraitUri());
 
 //        appbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
 //            @Override
@@ -155,7 +168,7 @@ public class CreateCharacterFragment extends Fragment implements Toolbar.OnMenuI
                             // Create the File where the photo should go
                             File photoFile = null;
                             try {
-                                photoFile = createImageFile();
+                                photoFile = createImageFile("IMG_");
                             } catch (IOException e) {
                                 // Error occurred while creating the File
                                 e.printStackTrace();
@@ -188,10 +201,10 @@ public class CreateCharacterFragment extends Fragment implements Toolbar.OnMenuI
 
     String mCurrentPhotoPath;
 
-    private File createImageFile() throws IOException {
+    private File createImageFile(String prefix) throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "CHARACTER_" + timeStamp;
+        String imageFileName = prefix + timeStamp;
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
@@ -209,6 +222,91 @@ public class CreateCharacterFragment extends Fragment implements Toolbar.OnMenuI
         Uri contentUri = Uri.fromFile(f);
         mediaScanIntent.setData(contentUri);
         getActivity().sendBroadcast(mediaScanIntent);
+    }
+
+
+    //Called when an image is selected from the camera or the gallery, and lets you crop it into an icon
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) return;
+        if (data != null) photoUri = data.getData();
+
+        try {
+            //Measure the size of the image without loading it into memory
+            InputStream input = getActivity().getContentResolver().openInputStream(photoUri);
+            BitmapFactory.Options bitmapInfo = new BitmapFactory.Options();
+            bitmapInfo.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(input, null, bitmapInfo);
+            input.close();
+            if ((bitmapInfo.outWidth == -1) || (bitmapInfo.outHeight == -1)) return;
+
+            //Get screen size, and find ratio of image size to screen size
+            WindowManager wm = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
+            Point size = new Point();
+            wm.getDefaultDisplay().getSize(size);
+            wm = null;
+            double ratio = Math.max(bitmapInfo.outHeight / (size.y), bitmapInfo.outWidth / (size.x));
+
+            //Scale image size down to match screen size
+            BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+            bitmapOptions.inSampleSize = (int) ratio;
+            bitmapOptions.inPreferredConfig= Bitmap.Config.ARGB_8888;//optional
+            input = getActivity().getContentResolver().openInputStream(photoUri);
+            final Bitmap bitmap = BitmapFactory.decodeStream(input, null, bitmapOptions);
+            input.close();
+
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+            final View cropImageView = inflater.inflate(R.layout.dialog_crop_image, null);
+            cropper = (CropImageView) cropImageView.findViewById(R.id.cropImageView);
+            cropper.setImageBitmap(bitmap);
+
+            new AlertDialog.Builder(getActivity())
+                    .setTitle(getResources().getString(R.string.crop_image_portrait))
+                    .setView(cropImageView)
+                    .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            imagePortrait.setImageBitmap(cropper.getCroppedImage());
+                            gameCharacter.setPortraitUri(Uri.parse(MediaStore.Images.Media.insertImage(
+                                    getActivity().getContentResolver(), cropper.getCroppedImage(), gameCharacter.getCharacterName(), gameCharacter.getGameSystem())));
+                            Log.i(null, "portrait uri == "+gameCharacter.getPortraitUri()+" and as a string: "+gameCharacter.getPortraitUri().toString());
+                            setIcon(bitmap);
+                        }
+                    })
+                    .setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            setIcon(bitmap);
+                        }
+                    }).show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void setIcon(Bitmap bitmap){
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        final View cropImageView = inflater.inflate(R.layout.dialog_crop_image, null);
+        cropper = (CropImageView) cropImageView.findViewById(R.id.cropImageView);
+        cropper.setImageBitmap(bitmap);
+
+        new AlertDialog.Builder(getActivity())
+                .setTitle(getResources().getString(R.string.crop_image_icon))
+                .setView(cropImageView)
+                .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Bitmap croppedBitmap = cropper.getCroppedImage();
+                        int circleImageSize = (int) getResources().getDimension(R.dimen.circle_icon_size);
+                        croppedBitmap = Bitmap.createScaledBitmap(croppedBitmap, circleImageSize, circleImageSize, true);
+                        gameCharacter.setCharacterIcon(croppedBitmap);
+                    }
+                })
+                .setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                }).show();
     }
 
 
