0f730a97e9b8c8d96c18e52c22b5921f163470bf
Amagi82 <amagi82@gmail.com>
2015-07-08 02:57:54 -0700

    add tab fragment

--- app/src/main/java/amagi82/modularcharactersheetcreator/fragments/TabFragment.java
+++ app/src/main/java/amagi82/modularcharactersheetcreator/fragments/TabFragment.java
@@ -1,8 +1,50 @@
 package amagi82.modularcharactersheetcreator.fragments;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bluelinelabs.logansquare.LoganSquare;
+
+import java.io.IOException;
+import java.util.List;
+
+import amagi82.modularcharactersheetcreator.R;
+import amagi82.modularcharactersheetcreator.adapters.SheetAdapter;
+import amagi82.modularcharactersheetcreator.models.Sheet;
+import amagi82.modularcharactersheetcreator.models.modules.Module;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class TabFragment extends Fragment {
+
+    @Bind(R.id.recycler_view) RecyclerView recyclerView;
+    private int spanCount = 3;
+    private Sheet sheet;
+
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_tab, container, false);
+        ButterKnife.bind(this, rootView);
+
+        try {
+            sheet = LoganSquare.parse(getArguments().getString("sheet"), Sheet.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        List<Module> modules = sheet.getModules();
+
+        StaggeredGridLayoutManager staggeredGrid = new StaggeredGridLayoutManager(spanCount, StaggeredGridLayoutManager.VERTICAL);
+        staggeredGrid.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_NONE);
+        recyclerView.setLayoutManager(staggeredGrid);
+        SheetAdapter adapter = new SheetAdapter(getResources(), modules);
+        recyclerView.setAdapter(adapter);
+        return rootView;
+    }
 
 
 }
