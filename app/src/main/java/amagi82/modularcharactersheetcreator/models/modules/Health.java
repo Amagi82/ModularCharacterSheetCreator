1633dc37afe1c510c174d360c5787a4a74cffcea
Amagi82 <Amagi82@gmail.com>
2015-10-22 00:32:58 -0700

    convert Modules to AutoParcel

--- app/src/main/java/amagi82/modularcharactersheetcreator/models/modules/Health.java
+++ app/src/main/java/amagi82/modularcharactersheetcreator/models/modules/Health.java
@@ -1,4 +1,33 @@
 package amagi82.modularcharactersheetcreator.models.modules;
 
-public class Health {
+import android.os.Parcelable;
+
+import auto.parcel.AutoParcel;
+
+@AutoParcel
+public abstract class Health implements Parcelable{
+    public abstract int bashing();
+    public abstract int lethal();
+    public abstract int agg();
+    public abstract int numBoxes();
+
+    public Health() {}
+
+    @AutoParcel.Builder
+    public abstract static class Builder {
+        public abstract Builder bashing(int bashing);
+        public abstract Builder lethal(int lethal);
+        public abstract Builder agg(int agg);
+        public abstract Builder numBoxes(int numBoxes);
+        public abstract Health build();
+
+        Builder() {}
+    }
+
+    public static Builder builder() {
+        return new AutoParcel_Health.Builder()
+                .numBoxes(7);
+    }
+
+    public abstract Builder toBuilder();
 }
