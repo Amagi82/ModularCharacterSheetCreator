4ea882a62cd52be152b36dc6f8b37f5347a7865c
Amagi82 <Amagi82@gmail.com>
2016-04-12 00:34:07 -0700

    Transition to Kotlin

--- app/src/main/java/amagi82/modularcharactersheetcreator/models/modules/Module.kt
+++ app/src/main/java/amagi82/modularcharactersheetcreator/models/modules/Module.kt
@@ -1,17 +1,11 @@
 package amagi82.modularcharactersheetcreator.models.modules
 
-import android.os.Parcelable
 import android.support.annotation.IntDef
-
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
-
-import auto.parcelgson.AutoParcelGson
 import nz.bradcampbell.paperparcel.PaperParcel
 import nz.bradcampbell.paperparcel.PaperParcelable
 
 @PaperParcel
-data class Module(@Type val type: Int, @SpanCount val spanCount: Int = 3, val title: String = "", val textBody: String? = null, val stat: Stat? = null, val statBlock: List<Stat>? = null, val health: Health? = null, val blood: Blood? = null, val imageUri: String? = null) : PaperParcelable {
+data class Module private constructor(@Type val type: Int, @SpanCount val spanCount: Int = ONE, val title: String = "", val textBody: String? = null, val stat: Stat? = null, val statBlock: List<Stat>? = null, val health: Health? = null, val blood: Blood? = null, val imageUri: String? = null) : PaperParcelable {
     @JvmField val CREATOR = PaperParcelable.Creator(Module::class.java)
 
     @kotlin.annotation.Retention(AnnotationRetention.SOURCE)
@@ -25,65 +19,19 @@ data class Module(@Type val type: Int, @SpanCount val spanCount: Int = 3, val ti
 
     companion object {
 
-        fun createHeader(title: String): Module {
-            return builder().type(HEADER_MODULE).title(title).spanCount(FULL).build()
-        }
+        fun header(title: String, spanCount: Int = FULL) = Module(type = HEADER_MODULE, title = title, spanCount = spanCount)
 
-        fun createHeader(title: String, @SpanCount spanCount: Int): Module {
-            return builder().type(HEADER_MODULE).title(title).spanCount(spanCount).build()
-        }
+        fun text(title:String, textBody: String, spanCount: Int = ONE) = Module(TEXT_MODULE, title = title, textBody = textBody, spanCount = spanCount)
 
-        fun createText(title: String, textBody: String): Module {
-            return builder().type(TEXT_MODULE).title(title).textBody(textBody).build()
-        }
+        fun stat(title: String, textBody: String, stat: Stat, spanCount: Int = ONE) = Module(STAT_MODULE, title = title, textBody = textBody, stat = stat, spanCount = spanCount)
 
-        fun createText(title: String, textBody: String, @SpanCount spanCount: Int): Module {
-            return builder().type(TEXT_MODULE).title(title).textBody(textBody).spanCount(spanCount).build()
-        }
+        fun statBlock(title: String, textBody: String? = null, statBlock: List<Stat>, spanCount: Int = ONE) = Module(STAT_BLOCK_MODULE, title = title, textBody = textBody, statBlock = statBlock, spanCount = spanCount)
 
-        fun createStat(title: String, textBody: String, stat: Stat): Module {
-            return builder().type(STAT_MODULE).title(title).textBody(textBody).stat(stat).spanCount(TWO).build()
-        }
+        fun health(title: String, health: Health, spanCount: Int = ONE) = Module(HEALTH_MODULE, title = title, health = health, spanCount = spanCount)
 
-        fun createStat(title: String, textBody: String, stat: Stat, @SpanCount spanCount: Int): Module {
-            return builder().type(STAT_MODULE).title(title).textBody(textBody).stat(stat).spanCount(spanCount).build()
-        }
+        fun blood(title: String, blood: Blood, spanCount: Int = ONE) = Module(BLOOD_MODULE, title = title, blood = blood, spanCount = spanCount)
 
-        fun createStatBlock(title: String, statBlock: List<Stat>): Module {
-            return builder().type(STAT_BLOCK_MODULE).title(title).statBlock(statBlock).build()
-        }
-
-        fun createStatBlock(title: String, textBody: String, statBlock: List<Stat>): Module {
-            return builder().type(STAT_BLOCK_MODULE).title(title).textBody(textBody).statBlock(statBlock).build()
-        }
-
-        fun createStatBlock(title: String, textBody: String, statBlock: List<Stat>, @SpanCount spanCount: Int): Module {
-            return builder().type(STAT_BLOCK_MODULE).title(title).textBody(textBody).statBlock(statBlock).spanCount(spanCount).build()
-        }
-
-        fun createHealth(title: String, health: Health): Module {
-            return builder().type(HEALTH_MODULE).title(title).health(health).build()
-        }
-
-        fun createHealth(title: String, health: Health, @SpanCount spanCount: Int): Module {
-            return builder().type(HEALTH_MODULE).title(title).health(health).spanCount(spanCount).build()
-        }
-
-        fun createBlood(title: String, blood: Blood): Module {
-            return builder().type(BLOOD_MODULE).title(title).blood(blood).build()
-        }
-
-        fun createBlood(title: String, blood: Blood, @SpanCount spanCount: Int): Module {
-            return builder().type(BLOOD_MODULE).title(title).blood(blood).spanCount(spanCount).build()
-        }
-
-        fun createImage(title: String, imageUri: String, @SpanCount spanCount: Int): Module {
-            return builder().type(IMAGE_MODULE).title(title).imageUri(imageUri).spanCount(spanCount).build()
-        }
-
-        internal fun builder(): Builder {
-            return AutoParcelGson_Module.Builder().title("").spanCount(ONE)
-        }
+        fun image(title: String, imageUri: String, spanCount: Int = ONE) = Module(IMAGE_MODULE, title = title, imageUri = imageUri, spanCount = spanCount)
 
         const val HEADER_MODULE = 1
         const val TEXT_MODULE = 2
